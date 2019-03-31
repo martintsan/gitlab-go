84eaec9e52558ba2e5363cf81773c3210a0850f2
Martin Nett <martin_nett@icloud.com>
2019-03-31 11:29:16 +0800

    Updated style

--- src/languages/zh_CN.js
+++ src/languages/zh_CN.js
@@ -2,10 +2,10 @@ const en = {
   "common-login": "立即登录",
   "common-no-more": "已经到底了",
 
-  "login-welcome": "欢迎使用",
+  "login-welcome": "欢迎使用 GitLab Go",
   "login-tips": "根据 GitLab API 的使用方式，您可能需要提供 Private Token 来登录。你可以在你的资料页面来创建 Private Token。",
   "login-show-more": "了解更多",
-  "login-gitlab-tips": "<p class='pargh'>1）登录 GitLab；</p><p class='pargh'>2）在右上角点击你的头像并选择<b>设置</b>；</p><p class='pargh'>3）在<b>用户设置</b>菜单上，选择<b>访问秘钥</b>；</p><p class='pargh'>4）为你的秘钥选择一个名字并可设置一个有效期；</p><p class='pargh'>5) 选择访问范围；</p><p class='pargh'>6) 点击<b>创建个人</b>访问秘钥；</p><p class='pargh'>7) 在安全的地方保存你的个人访问秘钥，一旦你离开或者刷新本页面，你将不能再访问到它。</p>",
+  "login-gitlab-tips": "<p class='pargh'>1）登录 GitLab；</p><p class='pargh'>2）在右上角点击你的头像并选择<b>设置</b>；</p><p class='pargh'>3）在<b>用户设置</b>菜单上，选择<b>访问秘钥</b>；</p><p class='pargh'>4）为你的秘钥选择一个名字并可设置一个有效期；</p><p class='pargh'>5）选择访问范围；</p><p class='pargh'>6）点击<b>创建个人</b>访问秘钥；</p><p class='pargh'>7）在安全的地方保存你的个人访问秘钥，一旦你离开或者刷新本页面，你将不能再访问到它。</p>",
   "login-token": "请输入您的 Private Token",
 
   "idx-last-updated": "{date}更新",
